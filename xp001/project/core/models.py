10940422401ec776f206e6bf0b85eaad798e688a
Henrique Bastos <henrique@bastos.net>
2022-11-17 22:22:42 -0300

    Working example.

--- xp001/project/core/models.py
+++ xp001/project/core/models.py
@@ -1,51 +1,123 @@
 from django.db import models
 
 
-class IdentificationTypes(models.TextChoices):
-    BRCPF = "BrCpf", "CPF"
-    BRCNPJ = "BrCnpj", "CNPJ"
-    BRRG = "BrRg", "RG"
+class IdentException(Exception):
+    pass
 
 
-class BadCustomer(models.Model):
-    identification_value = models.CharField(max_length=256)
-    identification_type = models.CharField(choices=IdentificationTypes.choices, max_length=32)
+class IdentAlreadyRegistered(IdentException):
+    def __init__(self, klass):
+        super().__init__(f"Ident {klass!r} is already registered.")
+
+
+class IdentNotFound(IdentException):
+    def __init__(self, kind):
+        super().__init__(f"Ident of kind {kind!r} is not registered.")
+
+
+class IdentTypeError(IdentException):
+    def __init__(self, klass):
+        super().__init__(f"Object type {klass!r} not of a subclass of Identification.")
+
+
+class IdentInvalid(IdentException):
+    def __init__(self, value):
+        super().__init__(f"Identification value is invalid: {value}.")
 
 
 class Identification(str):
-    def from_type(type, value):
-        if (type == "BrCpf"):
-            return Cpf()
-        if (type == "BrCnpj"):
-            return Cnpj()
+    kinds = {}
+
+    @property
+    def kind(self):
+        return type(self).__name__.lower()
+
+    @staticmethod
+    def as_key(kind, country=None):
+        return kind if country is None else (kind, country)
+
+    @classmethod
+    def name(cls):
+        return cls.__name__.lower()
+
+    @classmethod
+    def of_kind(cls, kind, value, country=None):
+        key = cls.as_key(kind, country)
+
+        if klass := cls.kinds.get(key):
+            return klass(value)
+        else:
+            raise IdentNotFound(kind)
+
+    @classmethod
+    def register(cls, klass, country=None):
+        if not issubclass(klass, cls):
+            raise IdentTypeError(klass)
+
+        if klass in cls.kinds:
+            raise IdentAlreadyRegistered(klass)
+
+        cls.kinds[cls.as_key(klass.name(), country)] = klass
+
+    @classmethod
+    def choices(cls):
+        return list(cls.kinds.items())
+
+
+class CPFInvalid(IdentInvalid):
+    pass
+
 
 class Cpf(Identification):
-    pass
+    def __new__(cls, string):
+        if not cls.checksum(string):
+            raise CPFInvalid(string)
+
+        return super().__new__(cls, string)
+
+    def __format__(self, spec):
+        if spec == ".":
+            return "{0}.{1}.{2}-{3}".format(self[:3], self[3:6], self[6:9], self[9:11])
+        else:
+            return self
+
+    @staticmethod
+    def checksum(value):
+        """
+        CPF Checksum algorithm.
+        """
+
+        def dv(partial):
+            s = sum(b * int(v) for b, v in zip(range(len(partial) + 1, 1, -1), partial))
+            return s % 11
+
+        dv1 = 11 - dv(value[:9])
+        q2 = dv(value[:10])
+        dv2 = 11 - q2 if q2 >= 2 else 0
+
+        return dv1 == int(value[9]) and dv2 == int(value[10])
+
 
 class Cnpj(Identification):
     pass
 
-class IdentificationField(models.Field):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.field_value = models.CharField(max_length=256)
-        self.field_type = models.CharField(max_length=32, choices=IdentificationTypes.choices)
 
-    def contribute_to_class(self, cls, name, private_only=True):
-        self.field_type_name = f"{name}_type"
-        self.field_value_name = f"{name}_value"
-        cls.add_to_class(self.field_type_name, self.field_type)
-        cls.add_to_class(self.field_value_name, self.field_value)
-        super().contribute_to_class(cls, name, private_only)
+Identification.register(Cpf)
+Identification.register(Cnpj)
 
 
-    def __set__(self, instance, value):
-        if not isinstance(value, Identification):
-            raise TypeError(f"Expected type Identification, but received {type(value)}.")
+class Customer(models.Model):
+    identification_value = models.CharField(max_length=256)
+    identification_kind = models.CharField(choices=Identification.choices(), max_length=32)
 
-        setattr(instance, self.field_value_name, str(value))
-        setattr(instance, self.field_type_name, type(value).__name__)
+    @property
+    def identification(self):
+        return Identification.of_kind(self.identification_kind, self.identification_value)
 
+    @identification.setter
+    def identification(self, ident):
+        if not isinstance(ident, Identification):
+            raise IdentTypeError(ident)
 
-class GoodCustomer(models.Model):
-    identification = IdentificationField()
+        self.identification_kind = ident.kind
+        self.identification_value = ident
--- xp001/project/core/tests/test_core.py
+++ xp001/project/core/tests/test_core.py
@@ -1,32 +1,23 @@
 import pytest
-from project.core.models import GoodCustomer, BadCustomer, IdentificationTypes, Cpf, Identification, Cnpj
+from project.core.models import Customer, Cpf, Identification, Cnpj
 
 
 @pytest.mark.django_db
 class TestCustomerModel:
 
-    def test_with_metacolumn(self):
-        id_type = IdentificationTypes.BRCPF
-        id_value = "11144477735"
-        c = BadCustomer.objects.create(identification_type=id_type, identification_value=id_value)
-
-        assert c.pk
-        assert c.identification_type == id_type
-        assert c.identification_value == id_value
-
-    def test_done_right(self):
+    def test_model(self):
         ident = Cpf("11144477735")
-        c = GoodCustomer.objects.create(identification=ident)
+        c = Customer.objects.create(identification=ident)
 
         assert c.pk
         assert type(c.identification) == Cpf
         assert c.identification == "11144477735"
+        assert f"{c.identification:.}" == "111.444.777-35"
 
     def test_factory(self):
-        ident = Identification.from_type("BrCpf", "11144477735")
-        assert type(ident) == Cpf
-        assert issubclass(Cpf, Identification)
+        print(Identification.kinds)
+        ident = Identification.of_kind("cpf", "11144477735")
+        assert isinstance(ident, Cpf)
 
-        ident_cnpj = Identification.from_type("BrCnpj", "11144477735")
-        assert type(ident_cnpj) == Cnpj
-        assert issubclass(Cnpj, Identification)
+        ident = Identification.of_kind("cnpj", "01234567890123")
+        assert isinstance(ident, Cnpj)
